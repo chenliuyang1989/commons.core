5bea9e6ee346ae74e5bcb8ba4d43b02a09629a98
ericschen <ericschen@lexinfintech.com>
2018-06-17 16:27:14 +0800

    代码初始化

--- src/main/java/com/lumi/aiot/common/customerThread/ThreadFactoryImpl.java
+++ src/main/java/com/lumi/aiot/common/customerThread/ThreadFactoryImpl.java
@@ -4,6 +4,7 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
+ * 自定义线程factory，命名清晰，便于后去管理
  * @author eric
  */
 public class ThreadFactoryImpl implements ThreadFactory {
